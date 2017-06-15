9333f881fabf12aa2bd63dce071994091e27795c
David Beauchamp <dbeauchamp@makermedia.com>
2017-06-15 14:26:30 +0100

    Adding a sponsor slider to the home page that uses the sponsors template

--- wp-content/themes/makercamp-theme/home.php
+++ wp-content/themes/makercamp-theme/home.php
@@ -142,6 +142,107 @@
     <div class="triangle-block"></div>
   </section>
 
+
+  <?php
+  $sponsor_pages = get_pages(array(
+    'meta_key' => '_wp_page_template',
+    'meta_value' => 'page-sponsors.php'
+  ));
+  foreach($sponsor_pages as $sponsor_page) {
+    $sponsor_ID = $sponsor_page->ID;
+  }
+  $sponsor_panel_field_3 = get_sub_field('become_a_sponsor_button', $sponsor_ID);
+
+  // check if the nested repeater field has rows of data
+  if( have_rows('sponsors', $sponsor_ID)) {
+    echo '
+    <section class="sponsor-slide">
+      <div class="container">
+        <div class="row sponsor_panel_title">
+          <div class="col-xs-12 text-center">
+            <div class="title-w-border-r">
+              <h2 class="sponsor-slide-title">'. get_the_title( $sponsor_ID ) .'</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12">
+            <div id="carousel-sponsors-slider" class="carousel slide" data-ride="carousel">
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">';
+                // loop through the rows of data
+                while ( have_rows('sponsors', $sponsor_ID) ) {
+                  the_row();
+                  $sponsor_group_title = get_sub_field('sponsor_group_title'); //Sponsor group title
+
+                  if( get_row_layout() == 'sponsors_with_image' ) {
+                    $sub_field_3 = get_sub_field('sponsors_image_size'); //size option
+
+                    // check if the nested repeater field has rows of data
+                    if( have_rows('sponsors_with_image') ) {
+                      echo '
+                      <div class="item">
+                        <div class="row spnosors-row">
+                          <div class="col-xs-12">';
+                            if(!empty($sponsor_group_title)) {
+                              echo '<h4 class="text-center sponsors-type">' . $sponsor_group_title . '</h4>';
+                            }
+                            echo '
+                            <div class="faire-sponsors-box">';
+
+                              // loop through the rows of data
+                              while ( have_rows('sponsors_with_image') ) {
+                                the_row();
+
+                                $sub_field_1 = get_sub_field('image'); //Photo
+                                $sub_field_2 = get_sub_field('url'); //URL
+                                $photon = get_fitted_remote_image_url($sub_field_1['url'], 300, 300);
+
+                                echo '<div class="' . $sub_field_3 . '">';
+                                if( $sub_field_2 ) {
+                                  echo '<a href="' . $sub_field_2 . '" target="_blank">';
+                                }
+                                echo '<img src="' . $photon . '" alt="Maker Faire sponsor logo" class="img-responsive" />';
+                                if( $sub_field_2 ) {
+                                  echo '</a>';
+                                }
+                                echo '</div>';
+                              }
+                            echo '
+                            </div>
+                          </div>
+                        </div>
+                      </div>';
+                    } // end if image
+                } // end row layout
+              }
+              echo '
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row sponsor_panel_bottom">
+          <div class="col-xs-12 text-center">
+            <p>';
+              $become_a_sponsor_button = get_field('become_a_sponsor_button', $sponsor_ID);
+              if($become_a_sponsor_button) {
+                echo '<a href="' . $become_a_sponsor_button . '">Become a Sponsor</a><span>&bull;</span>';
+              }
+              echo '
+              <a href="/sponsors">All Sponsors</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script>
+      jQuery(".sponsor-slide .carousel-inner .item:first-child").addClass("active");
+      jQuery(function() {
+        var title = jQuery(".item.active .sponsors-type").html();
+      });
+    </script>';
+  } ?>
+
   <?php echo stuff_for_sale_panel(); ?>
 
 <?php get_footer(); ?>
