b820aee81962d949ce9a82a920b53adc88a200ec
jaz <jazmine@makermedia.com>
2017-08-30 15:30:30 -0700

    Revert "clearing out endif statements for better readability - but did something break in the process?"
    This reverts commit 74065522091670fde3689f1ddeca7d1ea812f255.

--- wp-content/themes/makercamp-theme/page-affiliate-program.php
+++ wp-content/themes/makercamp-theme/page-affiliate-program.php
@@ -4,77 +4,85 @@
 
   <section class="host-a-camp-hero">
     <article>
-      <h1><?php
+      <?php
         $host_hero_title = makercamp_defaults_customizer('host_hero_title');
-        if (!empty($host_hero_title))
-       		echo $host_hero_title; 
-		  ?>
-       </h1>
-       <p><?php
+        if (!empty($host_hero_title)) :
+      ?>
+      <h1><?php echo $host_hero_title ?></h1>
+      <?php
+        endif;
+
         $host_hero_text = makercamp_defaults_customizer('host_hero_text');
-        if (!empty($host_hero_text)) {
-			echo $host_hero_text
-		?>
-		</p>
-        <?php   
+        if (!empty($host_hero_text)) :
+      ?>
+      <p><?php echo $host_hero_text ?></p>
+      <?php
+          endif;
+
         $host_hero_link = makercamp_defaults_customizer('host_hero_link');
-			?>
-		<a class="apply-now" href="<?php echo $host_hero_link ?>" target="_blank">
-       	<?php
-        if (!empty($host_hero_link)) {
-          	$host_hero_link_title = makercamp_defaults_customizer('host_hero_link_title');
-          	if (!empty($host_hero_link_title))
-            	echo $host_hero_link_title;
-		}
-		?></a>
+        if (!empty($host_hero_link)) :
+      ?>
+      <a class="apply-now" href="<?php echo $host_hero_link ?>" target="_blank">
+        <?php
+          $host_hero_link_title = makercamp_defaults_customizer('host_hero_link_title');
+          if (!empty($host_hero_link_title)) :
+            echo $host_hero_link_title;
+          endif;
+        ?>
+      </a>
+      <?php endif; ?>
     </article>
   </section>
 
   <section class="plane-future">
     <div class="container-fluid">
-      <h1><?php $host_first_section_title = makercamp_defaults_customizer('host_first_section_title');
-      if (!empty($host_first_section_title))
-       echo $host_first_section_title;
-		  ?>
-      </h1>
+      <?php $host_first_section_title = makercamp_defaults_customizer('host_first_section_title');
+      if (!empty($host_first_section_title)) :
+        ?>
+        <h1><?php echo $host_first_section_title; ?></h1>
+      <?php endif; ?>
 
       <ul class="plane-future-steps">
         <li>
           <span class="first-step">1</span>
-          <p><?php $host_first_section_first_text = makercamp_defaults_customizer('host_first_section_first_text');
-          if (!empty($host_first_section_first_text))
-           	echo $host_first_section_first_text;
-          	?>
-		  </p>
+
+          <?php $host_first_section_first_text = makercamp_defaults_customizer('host_first_section_first_text');
+          if (!empty($host_first_section_first_text)) :
+            ?>
+            <p><?php echo $host_first_section_first_text; ?></p>
+          <?php endif; ?>
 
         </li>
         <li>
           <span class="second-step">2</span>
-          <p><?php $host_first_section_second_text = makercamp_defaults_customizer('host_first_section_second_text');
-          if (!empty($host_first_section_second_text)) 
-           echo $host_first_section_second_text;
-          	?>
-		  </p>
+
+          <?php $host_first_section_second_text = makercamp_defaults_customizer('host_first_section_second_text');
+          if (!empty($host_first_section_second_text)) :
+            ?>
+            <p><?php echo $host_first_section_second_text; ?></p>
+          <?php endif; ?>
 
         </li>
       </ul>
       <ul class="plane-future-steps">
         <li>
-		  <span class="third-step">3</span>
-          <p><?php $host_first_section_third_text = makercamp_defaults_customizer('host_first_section_third_text');
-          if (!empty($host_first_section_third_text))
-          	echo $host_first_section_third_text;
-          ?>
-		  </p>
+          <span class="third-step">3</span>
+
+          <?php $host_first_section_third_text = makercamp_defaults_customizer('host_first_section_third_text');
+          if (!empty($host_first_section_third_text)) :
+            ?>
+            <p><?php echo $host_first_section_third_text; ?></p>
+          <?php endif; ?>
 
         </li>
         <li>
           <span class="fourth-step">4</span>
-          <p><?php $host_first_section_fourth_text = makercamp_defaults_customizer('host_first_section_fourth_text');
-          if (!empty($host_first_section_fourth_text))
-          	echo $host_first_section_fourth_text;
-          	?>
-		  </p>
+
+          <?php $host_first_section_fourth_text = makercamp_defaults_customizer('host_first_section_fourth_text');
+          if (!empty($host_first_section_fourth_text)) :
+            ?>
+            <p><?php echo $host_first_section_fourth_text; ?></p>
+          <?php endif; ?>
 
         </li>
       </ul>
