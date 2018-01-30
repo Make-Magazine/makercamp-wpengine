380d9a078753585480e2b31e5e5d7afc9e71a678
David Beauchamp <dbeauchamp@makermedia.com>
2018-01-30 13:59:16 -0500

    Trying to figure out why the second modal won’t ajax the form

--- wp-content/themes/makercamp-theme/assets/js/script.js
+++ wp-content/themes/makercamp-theme/assets/js/script.js
@@ -695,9 +695,9 @@ $(document).ready(function () {
     }
   });
   // Thank you modal with extra newsletter options
-  $(document).on('submit', '.whatcounts-signup2', function (e) {
+  $('.fancybox-wrap').on('click', '.ghost-button-black', function (e) {
     e.preventDefault();
-    $.post('https://secure.whatcounts.com/bin/listctrl', $('.fancybox-wrap .whatcounts-signup2').serialize());
+    $.post('https://secure.whatcounts.com/bin/listctrl', $('.whatcounts-signup2').serialize());
     $('.fancybox-thx').hide();
     $('.nl-thx-p2').trigger('click');
   });
