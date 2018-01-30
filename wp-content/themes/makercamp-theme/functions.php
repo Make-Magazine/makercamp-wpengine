88bc597ae43f4000855878db8db07905c95e953c
David Beauchamp <dbeauchamp@makermedia.com>
2018-01-30 14:09:28 -0500

    cache busting js

--- wp-content/themes/makercamp-theme/functions.php
+++ wp-content/themes/makercamp-theme/functions.php
@@ -89,7 +89,7 @@ function makercamp_theme_scripts() {
   /* Custom CSS */
   wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/public/css/custom.min.css?v=4.8' );
   /* Custom JS */
-  wp_enqueue_script( 'script-js', get_template_directory_uri() . '/public/js/script.min.js?v=2.7', array('jquery'), '', true );
+  wp_enqueue_script( 'script-js', get_template_directory_uri() . '/public/js/script.min.js?v=2.8', array('jquery'), '', true );
   //wp_enqueue_script( 'makercamp_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
